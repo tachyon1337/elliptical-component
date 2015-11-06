4aa494fbcf666f8ac33620aed681618709f71b9a
Scott <dev@misonline.biz>
2015-11-06 09:23:08 -0500

    Updated build

--- bundle/elliptical.component.js
+++ bundle/elliptical.component.js
@@ -5023,23 +5023,55 @@
 (function (root, factory) {
     if (typeof module !== 'undefined' && module.exports) {
         //commonjs
-        module.exports = factory(require('elliptical-utils'),require('jquery-extensions'),require('./device'), require('./template'),
-            require('./transition'), require('./transform'),require('./utils'),require('./event'));
+        module.exports = factory(require('elliptical-utils'));
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['elliptical-utils'], factory);
+    } else {
+        // Browser globals (root is window)
+        root.elliptical.extensions=root.elliptical.extensions || {};
+        root.elliptical.extensions.options = factory(root.elliptical.utils);
+        root.returnExports = root.elliptical.extensions.options;
+    }
+}(this, function (utils) {
+
+    return {
+
+        _setOptionsFromAttribute:function(){
+            var options=(this.options) ? this.options.opts : this.opts;
+            if(options===undefined) return;
+            options=JSON.parse(options);
+            var src=(this.options) ? this.options : this;
+            Object.assign(src,options);
+        }
+
+
+    };
+}));
+
+//umd pattern
+
+(function (root, factory) {
+    if (typeof module !== 'undefined' && module.exports) {
+        //commonjs
+        module.exports = factory(require('elliptical-utils'),require('jquery-extensions'),require('./prototype.device'), require('./prototype.template'),
+            require('./prototype.transition'), require('./prototype.transform'),require('./prototype.utils'),
+            require('./prototype.event'),require('./prototype.options'));
     } else if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(['elliptical-utils','jquery-extensions','./prototype.device',
-            './prototype.template','./prototype.transition','./prototype.transform','./prototype.utils','./prototype.event'], factory);
+            './prototype.template','./prototype.transition','./prototype.transform','./prototype.utils','./prototype.event','./prototype.options'], factory);
     } else {
         // Browser globals (root is window)
         var e=root.elliptical.extensions;
         root.elliptical.extensions.base = factory(root.elliptical.utils,e.device,
-            e.template,e.transition,e.transform,e.utils,e.event);
+            e.template,e.transition,e.transform,e.utils,e.event,e.options);
         root.returnExports = root.elliptical.extensions.base;
     }
-}(this, function (utils,root,device,template,transition,transform,util,event) {
+}(this, function (utils,root,device,template,transition,transform,util,event,options) {
 
     var base={};
-    Object.assign(base,util,device,template,transition,transform,event);
+    Object.assign(base,util,device,template,transition,transform,event,options);
     return base;
 
 }));
@@ -10710,6 +10742,7 @@ return $.widget;
             }else{
                 this._created=true;
             }
+            this._setOptionsFromAttribute();
             this._publishLoaded();
             this._initElement();
             this.__onInit();
